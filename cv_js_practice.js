66c6d318d01f952f7c55ce1d1bc346ad8cf418bc
ambition <taisiaiueo@gmail.com>
2017-07-19 12:28:19 +0900

    canvas

---
+++ cv_js_practice.js
@@ -0,0 +1,21 @@
+var ctx;
+function init(){
+    var canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d"); //canvssの取得
+
+    ctx.strokeStyle = "#FF0000";
+    ctx.fillStyle = "#00FF00";
+    ctx.lineWidth = 10;
+    ctx.lineCap = "round";
+    ctx.shadowColor ="#000000";
+    ctx.shadowBlur = 20;
+
+
+    ctx.beginPath();
+    ctx.moveTo(100,100);
+    ctx.lineTo(300,150);
+    ctx.stroke();
+
+    ctx.fillRect(100,200,200,100);
+    ctx.strokeRect(100,200,200,100);
+}
